1586e22cb696374ffb9c6a1e7728634e3bee3a37
Jo@o Borges <joao.borges@jb.dev.br>
2025-02-10 17:54:21 -0300

    fix(dependencies): export http barrels

--- apps/base_app/features/dependencies/lib/dependencies.dart
+++ apps/base_app/features/dependencies/lib/dependencies.dart
@@ -2,6 +2,8 @@ library;
 
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:core/core.dart';
+export 'package:http_common/http_common.dart';
+export 'package:http_minimal/http_minimal.dart';
 export 'package:locator_common/locator_common.dart';
 export 'package:locator_native/locator_native.dart';
 export 'package:log_common/log_common.dart';
