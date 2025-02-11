4af27048f47cb63ae6a19d6a6dcdedde6b6cf58e
Jo@o Borges <joao.borges@jb.dev.br>
2025-02-11 06:24:04 -0300

    feat(home): home view responsive

--- apps/base_app/features/home/lib/src/view/home_widgets.dart
+++ apps/base_app/features/home/lib/src/view/home_widgets.dart
@@ -37,15 +37,15 @@ class _MotelList extends SizedBox {
 class _MotelCard extends Card {
   _MotelCard({required Motel motel})
       : super(
-          elevation: .5,
-          margin: const EdgeInsets.all(18),
+          elevation: .5.fontSize,
+          margin: EdgeInsets.all(18.fontSize),
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
+            padding: EdgeInsets.symmetric(vertical: 8.height),
             child: Column(
               children: [
                 ListTile(
                   leading: _Image(url: motel.image),
-                  title: _MotelTitle(name: motel.name),
+                  title: _MotelTitle(motel.name),
                 ),
                 _SuiteList(suites: motel.suites),
               ],
@@ -55,11 +55,14 @@ class _MotelCard extends Card {
 }
 
 class _MotelTitle extends Builder {
-  _MotelTitle({required String name})
+  _MotelTitle(String data)
       : super(
           builder: (context) => Text(
-            name,
-            style: Theme.of(context).textTheme.headlineMedium,
+            data,
+            style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                  fontSize: 30.fontSize,
+                  fontWeight: FontWeight.w300,
+                ),
           ),
         );
 }
@@ -67,7 +70,7 @@ class _MotelTitle extends Builder {
 class _SuiteList extends SizedBox {
   _SuiteList({required List<Suite> suites})
       : super(
-          height: 300,
+          height: 250.height,
           child: ListView.builder(
             itemCount: suites.length,
             shrinkWrap: true,
@@ -80,15 +83,43 @@ class _SuiteList extends SizedBox {
 class _SuiteCard extends Padding {
   _SuiteCard({required Suite suite})
       : super(
-          padding: const EdgeInsets.all(8),
+          padding: EdgeInsets.all(8.fontSize),
           child: Column(
             children: [
               _Image(url: suite.images[0]),
-              Text(suite.name),
-              Text(
+              SizedBox(height: 4.height),
+              _SuiteTitle(suite.name),
+              SizedBox(height: 4.height),
+              _SuiteSubtitle(
                 '${suite.periods[0].formattedTime} - ${suite.periods[0].value.toFormat}',
               ),
             ],
+          ),
+        );
+}
+
+class _SuiteTitle extends Builder {
+  _SuiteTitle(String data)
+      : super(
+          builder: (context) => Text(
+            data,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  fontSize: 12.fontSize,
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+        );
+}
+
+class _SuiteSubtitle extends Builder {
+  _SuiteSubtitle(String data)
+      : super(
+          builder: (context) => Text(
+            data,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontSize: 12.fontSize,
+                  fontWeight: FontWeight.bold,
+                ),
           ),
         );
 }
@@ -97,8 +128,8 @@ class _Image extends CachedNetworkImage {
   _Image({required String url})
       : super(
           imageUrl: url,
-          memCacheHeight: 250,
-          memCacheWidth: 250,
+          memCacheHeight: 250.fontSize.toInt(),
+          memCacheWidth: 250.fontSize.toInt(),
           fit: BoxFit.cover,
           placeholder: (_, __) => CircularProgressIndicator(
             color: Colors.grey[300]!,
